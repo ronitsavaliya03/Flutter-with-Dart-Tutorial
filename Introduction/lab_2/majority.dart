6da0a3c3b15c8851c0edaf4a89a6a56e336e81b5
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2024-12-10 20:01:23 +0530

    majority.dart

--- Introduction/lab_2/majority.dart
+++ Introduction/lab_2/majority.dart
@@ -1,9 +1,15 @@
-import 'dart:io';
+void main() {
+  List<int> nums = [2, 2, 1, 1, 1, 2, 2];
 
-void main(){
-  List a=[];
-  stdout.write("Enter -1 to break.");
+  int candidate = nums[0];
+  int count = 0;
 
-  
+  for (int num in nums) {
+    if (count == 0) {
+      candidate = num;
+    }
+    count += (num == candidate) ? 1 : -1;
+  }
 
+  print("The majority element is: $candidate");
 }
