ab57cb204e2286e047ddfd5cdb659a4df1de014c
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2024-12-10 20:06:09 +0530

    insert_list.dart

--- Introduction/lab_3/insert_list.dart
+++ Introduction/lab_3/insert_list.dart
@@ -1,5 +1,14 @@
-import 'dart:io';
+void main() {
+  List<int> array = [1, 2, 4, 5];
+  int element = 3;
+  int position = 2;
 
-void main(){
-  
+  array.add(0); 
+  for (int i = array.length - 1; i > position; i--) {
+    array[i] = array[i - 1];
+  }
+
+  array[position] = element;
+
+  print("Modified array: $array");
 }
