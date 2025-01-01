55bd9d43522cd5586dbbc90a13dd4a88650b04f2
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2025-01-01 20:50:57 +0530

    create the tab view demo

---
+++ UI Design, State Management, Navigation/lab_9/tab_view_demo.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_labs/UI%20Design,%20State%20Management,%20Navigation/lab_9/Tabs/about.dart';
+import 'package:flutter_labs/UI%20Design,%20State%20Management,%20Navigation/lab_9/Tabs/contact.dart';
+import 'package:flutter_labs/UI%20Design,%20State%20Management,%20Navigation/lab_9/Tabs/home.dart';
+
+class TabViewDemo extends StatefulWidget {
+  const TabViewDemo({super.key});
+
+  @override
+  State<TabViewDemo> createState() => _TabViewDemoState();
+}
+
+class _TabViewDemoState extends State<TabViewDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 3, child: Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(tabs: [
+          Tab(
+            text: "Home",
+          ),
+          Tab(
+            text: "Contact",
+          ),
+          Tab(
+            text: "About",
+          ),
+        ]),
+        actions: [
+          IconButton(onPressed: () {
+            showDialog(context: context, builder: (context) {
+              return AlertDialog(
+                title: Text("Are you sure?"),
+                content: Text("Content"),
+                actions: [
+                  ElevatedButton(onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Home()));
+                  }, child: Text("Ok")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Cancel"))
+                ],
+              );
+            });
+          }, icon: Icon(Icons.logout))
+        ],
+      ),
+      body: TabBarView(children: [Home(), Contact(), About()]),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text("Index")),
+            ListTile(
+              title: Text("Home"),
+              leading: Icon(Icons.home_outlined),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => Home(),
+                ));
+              },
+            ),
+            ListTile(
+              title: Text("About"),
+              leading: Icon(Icons.account_circle_outlined),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => About(),
+                ));
+              },
+            ),
+            ListTile(
+              title: Text("Contact"),
+              leading: Icon(Icons.contact_support_outlined),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => Contact(),
+                ));
+              },
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
