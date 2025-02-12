44444449106f68e7ebd2411f46f65dd3a4825b11
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2025-02-12 23:01:17 +0530

    fetch and print tasks

--- Database Connectivity & Navigation/lab_15/for_database.dart
+++ Database Connectivity & Navigation/lab_15/for_database.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_labs/Database%20Connectivity%20&%20Navigation/lab_15/my_database.dart';
+import 'package:new_flutter_labs/Database%20Connectivity%20&%20Navigation/lab_15/my_database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ForDatabase extends StatefulWidget {
   const ForDatabase({super.key});
@@ -9,17 +10,43 @@ class ForDatabase extends StatefulWidget {
 }
 
 class _ForDatabaseState extends State<ForDatabase> {
-  MyDatabase database=MyDatabase();
+  MyDatabase database = MyDatabase();
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    // deleteAll();
     selectAll();
   }
 
-  Future<void> selectAll() async{
-    await database.insertTasks();
-    List<Map<String, dynamic>> tasks=await database.selectAllTask();
+  Future<void> selectAll() async {
+    await database.insertCategory("categoryName");
+
+    Database db = await database.initDatabase();
+    List<Map<String, dynamic>> categories = await db.rawQuery(
+        "SELECT category_id FROM Categories WHERE category_name = ?", ["categoryName"]);
+
+    if (categories.isNotEmpty) {
+      int categoryId = categories.first["category_id"];
+
+      await database.insertTask({
+        "task_title": "hello",
+        "task_description": "Buy vegetables and fruits for the week",
+        "task_status": 0,
+        "category_id": categoryId, 
+      });
+
+      List<Map<String, dynamic>> tasks = await database.selectAllTasks();
+      List<Map<String, dynamic>> taskJoin = await database.selectAllTasksUsingJoin();
+
+      print("All Tasks: $tasks");
+      print("Using INNER JOIN: $taskJoin");
+    } else {
+      print("Category not found.");
+    }
+  }
+  Future<void> deleteAll()async{
+    database.deleteTable();
   }
   @override
   Widget build(BuildContext context) {
